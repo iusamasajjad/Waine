d2c43afd5d896a769d87cdeae28b19d14edf2a52
Usama Qureshi <usamaqureshi@192.168.1.7>
2023-08-26 22:05:47 +0500

    new pages added for dashboard

--- assets/script/script.js
+++ assets/script/script.js
@@ -26,3 +26,45 @@ function menuClose() {
   var x = document.getElementById("mobile-menu");
   x.style.width = "0%";
 }
+// =============== word-editor =============
+var editor = CKEDITOR.replace("editor1", {
+  width: "100%",
+  height: "700",
+  contentsCss: "",
+  //removePlugins : 'pastefromword',
+  extraAllowedContent:
+      "h1;div{border,background-color};table[align,cellspacing]{margin,width,border,border-*};tr;td{border-color,height,vertical-align,width};tbody;th;span{background-color,background,color,font-size,font-family};p{margin-left,margin-right,margin-top,margin-bottom}"
+});
+
+editor.on("pluginsLoaded", function (evt) {
+  evt.editor.filter.addTransformations([
+      [
+          {
+              element: "td",
+              left: function (el) {
+                  return el.name == "td";
+              },
+              right: function (el, tools) {
+                  if (el.attributes && el.attributes.valign) {
+                      el.styles["vertical-align"] = el.attributes.valign;
+                      delete el.attributes.valign;
+                  }
+              }
+          }
+      ],
+      [
+          {
+              element: "div",
+              left: function (el) {
+                  return el.name == "div";
+              },
+              right: function (el, tools) {
+                  if (el.styles && el.styles["background"]) {
+                      el.styles["background-color"] = el.styles["background"];
+                      delete el.styles["background"];
+                  }
+              }
+          }
+      ]
+  ]);
+});
